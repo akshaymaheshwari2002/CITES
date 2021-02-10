9fcd4267dc5ccc6928c03da47022211f2857bc09
Jyoti <jyoti.gupta@daffodilsw.com>
2021-02-10 11:07:56 +0530

    Installed react-native-vector-icons and modified some styles

--- src/styles/CommonStyles.js
+++ src/styles/CommonStyles.js
@@ -2,6 +2,15 @@ import {StyleSheet} from 'react-native';
 import {RawColors} from './Themes';
 
 export default StyleSheet.create({
+  shadowEffect: {
+    shadowColor: RawColors.black,
+    shadowOpacity: 0.3,
+    elevation: 5,
+    shadowOffset: {
+      height: 40,
+      width: 40,
+    },
+  },
   navigationHeader: {
     elevation: 0,
     shadowOpacity: 0,
