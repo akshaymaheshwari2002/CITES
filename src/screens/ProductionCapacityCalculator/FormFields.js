65aeabc2de0a6804a2de7891961f350730af3ebb
Pranjay <pranjay@daffodilsw.com>
2021-02-24 10:51:34 +0530

    added help modal in production capacity calculator

--- src/screens/ProductionCapacityCalculator/FormFields.js
+++ src/screens/ProductionCapacityCalculator/FormFields.js
@@ -1,6 +1,9 @@
+import {useDispatch} from 'react-redux';
+
 import getValidators from '@utils/FormValidators';
 import createIntl from '@utils/Intl';
 import {RawColors} from '@styles/Themes';
+import {setHelpText} from '@store/slices/sessionSlice';
 
 const resultFieldStyleProps = {
   fontWeight: 'bold',
@@ -8,7 +11,40 @@ const resultFieldStyleProps = {
   color: RawColors.black,
 };
 
+const helpText = {
+  countTotalBreedingFemale: [
+    'Number # of breeding females',
+    'The total number of breeding females observed or claimed to be at the facility. Enter as a whole number.',
+    'For example, 50.',
+  ],
+  percentageBreedingFemalePerSeason: [
+    'Mean percentage % of females breeding per season',
+    'The mean percentage (proportion) of females producing a clutch or litter per year. This information can be supplied by the facility, but should be confirmed using reliable external sources.',
+    'For example, enter 70% enter as 0.7.',
+  ],
+  countLitterPerYear: [
+    'Mean number # of litters / clutches per year',
+    'The mean number of litters or clutches produced by females per year. This information can be supplied by the facility, but should be confirmed using reliable external sources.',
+    'For example, enter as a whole number, such as 2.',
+  ],
+  countOffspringPerLitter: [
+    'Mean number # of offspring / eggs in litter / clutch',
+    'This information can be supplied by the facility, but should be confirmed using reliable external sources.',
+    'For example, enter as a whole number, such as 20.',
+  ],
+  percentageSurvivingInTwoWeek: [
+    'Mean percentage % of surviving after two weeks',
+    'The mean number of eggs or live offspring produced that survive two weeks after hatching or birth. Enter as a percentage (of offspring surviving).',
+    'For example, enter 80% as 0.8.',
+  ],
+  approximateYoungProducedPerYear: [
+    'Number # of young per year at the facility inspected',
+    'The estimated number of offspring the observed or claimed number of female stock can produce each year.',
+  ],
+};
+
 export default ({modeSelected}) => {
+  const dispatch = useDispatch();
   const {formatMessage} = createIntl();
   const {
     required,
@@ -37,6 +73,9 @@ export default ({modeSelected}) => {
       style:
         modeSelected === 2 ? resultFieldStyleProps : {color: RawColors.black},
       showHelpIcon: true,
+      onHelpIconPress: () => {
+        dispatch(setHelpText(helpText.countTotalBreedingFemale));
+      },
       editable: modeSelected === 1,
       keyboardType: 'number-pad',
     },
@@ -54,6 +93,9 @@ export default ({modeSelected}) => {
         },
       },
       showHelpIcon: true,
+      onHelpIconPress: () => {
+        dispatch(setHelpText(helpText.percentageBreedingFemalePerSeason));
+      },
       keyboardType: 'decimal-pad',
     },
     {
@@ -69,6 +111,9 @@ export default ({modeSelected}) => {
         },
       },
       showHelpIcon: true,
+      onHelpIconPress: () => {
+        dispatch(setHelpText(helpText.countLitterPerYear));
+      },
       keyboardType: 'number-pad',
     },
     {
@@ -84,6 +129,9 @@ export default ({modeSelected}) => {
         },
       },
       showHelpIcon: true,
+      onHelpIconPress: () => {
+        dispatch(setHelpText(helpText.countOffspringPerLitter));
+      },
       keyboardType: 'number-pad',
     },
     {
@@ -98,6 +146,9 @@ export default ({modeSelected}) => {
         },
       },
       showHelpIcon: true,
+      onHelpIconPress: () => {
+        dispatch(setHelpText(helpText.percentageSurvivingInTwoWeek));
+      },
       keyboardType: 'decimal-pad',
     },
     {
@@ -118,6 +169,9 @@ export default ({modeSelected}) => {
             }
           : {},
       showHelpIcon: true,
+      onHelpIconPress: () => {
+        dispatch(setHelpText(helpText.approximateYoungProducedPerYear));
+      },
       keyboardType: 'number-pad',
       editable: modeSelected === 2,
       style:
