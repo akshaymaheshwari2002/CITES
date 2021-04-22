4593d3e8542032c6b371214e4610fefa4aad6abd
Jyoti <jyoti.gupta@daffodilsw.com>
2021-04-22 17:17:48 +0530

    intl added

--- src/utils/Intl.js
+++ src/utils/Intl.js
@@ -5,6 +5,11 @@ import Config from '@config';
 import {store} from '@store';
 import messages_en from '@locale/en.json';
 import messages_vi from '@locale/vi.json';
+import messages_es from '@locale/es.json';
+import messages_id from '@locale/id.json';
+import messages_khmer from '@locale/km.json';
+import messages_lao from '@locale/lo.json';
+import messages_malay from '@locale/ms.json';
 
 const cache = createIntlCache();
 let intl;
@@ -12,6 +17,11 @@ let intl;
 export const getMessages = () => ({
   en: messages_en,
   vi: messages_vi,
+  es: messages_es,
+  id: messages_id,
+  khmer: messages_khmer,
+  lao: messages_lao,
+  malay: messages_malay,
 });
 
 export const getDeviceLocale = () => {
