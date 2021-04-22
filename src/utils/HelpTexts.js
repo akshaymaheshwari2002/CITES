2f0c819af49dc9ca0bd3bb727a50cf5e9877a428
ms-akshay <akshay26072000@gmail.com>
2021-04-22 15:44:01 +0530

    helpText

--- src/utils/HelpTexts.js
+++ src/utils/HelpTexts.js
@@ -1,164 +1,176 @@
 export default {
   countTotalBreedingFemale: [
-    {text: 'Number of breeding females', isBold: true},
+    {text: 'helpText.countTotalBreedingFemale.one', isBold: true},
     {
-      text:
-        'The total number of breeding females observed or claimed to be at the facility. Enter as a whole number.',
+      text: 'helpText.countTotalBreedingFemale.two',
       isBold: false,
     },
-    {text: 'For example, 50.', isBold: false},
+    {text: 'helpText.countTotalBreedingFemale.three', isBold: false},
   ],
   percentageBreedingFemalePerSeason: [
-    {text: 'Mean percentage of females breeding per season', isBold: true},
     {
-      text:
-        'The mean percentage (proportion) of females producing a clutch or litter per year. This information can be supplied by the facility, but should be confirmed using reliable external sources.',
+      text: 'helpText.percentageBreedingFemalePerSeason.one',
+      isBold: true,
+    },
+    {
+      text: 'helpText.percentageBreedingFemalePerSeason.two',
       isBold: false,
     },
-    {text: 'For example, enter 70% enter as 0.7.', isBold: false},
+    {
+      text: 'helpText.percentageBreedingFemalePerSeason.three',
+      isBold: false,
+    },
   ],
   countLitterPerYear: [
-    {text: 'Mean number of litters / clutches per year', isBold: true},
+    {text: 'helpText.countLitterPerYear.one', isBold: true},
     {
-      text:
-        'The mean number of litters or clutches produced by females per year. This information can be supplied by the facility, but should be confirmed using reliable external sources.',
+      text: 'helpText.countLitterPerYear.two',
       isBold: false,
     },
-    {text: 'For example, enter as a whole number, such as 2.', isBold: false},
+    {text: 'helpText.countLitterPerYear.three', isBold: false},
   ],
   countOffspringPerLitter: [
     {
-      text: 'Mean number of offspring / eggs in litter / clutch',
+      text: 'helpText.countOffspringPerLitter.one',
       isBold: true,
     },
     {
-      text:
-        'This information can be supplied by the facility, but should be confirmed using reliable external sources.',
+      text: 'helpText.countOffspringPerLitter.two',
       isBold: false,
     },
-    {text: 'For example, enter as a whole number, such as 20.', isBold: false},
+    {text: 'helpText.countOffspringPerLitter.three', isBold: false},
   ],
   percentageSurvivingInTwoWeek: [
-    {text: 'Mean percentage of surviving after two weeks', isBold: true},
+    {text: 'helpText.percentageSurvivingInTwoWeek.one', isBold: true},
     {
-      text:
-        'The mean number of eggs or live offspring produced that survive two weeks after hatching or birth. Enter as a percentage (of offspring surviving).',
+      text: 'helpText.percentageSurvivingInTwoWeek.two',
       isBold: false,
     },
-    {text: 'For example, enter 80% as 0.8.', isBold: false},
+    {
+      text: 'helpText.percentageSurvivingInTwoWeek.three',
+      isBold: false,
+    },
   ],
   approximateYoungProducedPerYear: [
     {
-      text: 'Number of young per year at the facility inspected',
+      text: 'helpText.approximateYoungProducedPerYear.one',
       isBold: true,
     },
     {
-      text:
-        'The estimated number of offspring the observed or claimed number of female stock can produce each year.',
+      text: 'helpText.approximateYoungProducedPerYear.two',
       isBold: false,
     },
   ],
   typeOfInspection: [
-    {text: 'Types of inspections', isBold: true},
+    {text: 'helpText.typeOfInspection.one', isBold: true},
     {
-      text: 'An',
+      text: 'helpText.typeOfInspection.two.text',
       isBold: false,
       subText: [
-        {val: ' initial inspection', isSubBold: true},
         {
-          val: ' is the first inspection of a facility.',
+          val: 'helpText.typeOfInspection.two.subText.one',
+          isSubBold: true,
+        },
+        {
+          val: 'helpText.typeOfInspection.two.subText.two',
           isSubBold: false,
         },
       ],
     },
 
     {
-      text: 'A',
+      text: 'helpText.typeOfInspection.three.text',
       isBold: false,
       subText: [
-        {val: ' routine inspection', isSubBold: true},
         {
-          val: ' of a facility is undertaken on a regular basis.',
+          val: 'helpText.typeOfInspection.three.subText.one',
+          isSubBold: true,
+        },
+        {
+          val: 'helpText.typeOfInspection.three.subText.two',
           isSubBold: false,
         },
       ],
     },
     {
-      text: 'A',
+      text: 'helpText.typeOfInspection.four.text',
       isBold: false,
       subText: [
-        {val: ' follow-up inspection', isSubBold: true},
         {
-          val:
-            ' is undertaken to ensure that a facility has complied with any issues raised on a previous initial or routine inspection.',
+          val: 'helpText.typeOfInspection.four.subText.one',
+          isSubBold: true,
+        },
+        {
+          val: 'helpText.typeOfInspection.four.subText.two',
           isSubBold: false,
         },
       ],
     },
   ],
   staffCurrentAtFacility: [
-    {text: 'Hours of employment', isBold: true},
+    {text: 'helpText.staffCurrentAtFacility.one', isBold: true},
     {
-      text:
-        'The number of staff employed will depend on the species cared for, the size of the captive-breeding and/or ranching operation and the complexity of the organization structure. Generally, the more specimens cared for, the more staff are required.',
+      text: 'helpText.staffCurrentAtFacility.two',
       isBold: false,
     },
     {
-      text: '',
+      text: 'helpText.staffCurrentAtFacility.three.text',
       isBold: false,
       subText: [
         {
-          val: 'Full-time',
+          val: 'helpText.staffCurrentAtFacility.three.subText.one',
           isSubBold: true,
         },
         {
-          val: ' employment is usually considered between 30-40 hours a week.',
+          val: 'helpText.staffCurrentAtFacility.three.subText.two',
           isSubBold: false,
         },
       ],
     },
     {
-      text: '',
+      text: 'helpText.staffCurrentAtFacility.four.text',
       isBold: false,
       subText: [
         {
-          val: 'Part-time',
+          val: 'helpText.staffCurrentAtFacility.four.subText.one',
           isSubBold: true,
         },
         {
-          val: ' employment is usually less than 30 hours a week.',
+          val: 'helpText.staffCurrentAtFacility.four.subText.two',
           isSubBold: false,
         },
       ],
     },
   ],
   professionalVeterinaryServices: [
-    {text: 'A veterinarian (vet)', isBold: true},
     {
-      text:
-        'also known as a veterinary surgeon or veterinary physician, is a professional who practices veterinary medicine by treating diseases, disorders, and injuries in non-human animals.',
+      text: 'helpText.professionalVeterinaryServices.one',
+      isBold: true,
+    },
+    {
+      text: 'helpText.professionalVeterinaryServices.two',
       isBold: false,
     },
   ],
   animalsOtherLocation: [
-    {text: 'Ensure all premises are inspected.', isBold: true},
     {
-      text:
-        'There may be several premises where animals are kept. Ensure that all premises are inspected, even if they are at a different location to the one you are currently inspecting.',
+      text: 'helpText.animalsOtherLocation.one',
+      isBold: true,
+    },
+    {
+      text: 'helpText.animalsOtherLocation.two',
       isBold: false,
     },
   ],
   formFour: [
     {
-      text:
-        'Facilities should have permits to keep the species observed during the inspection. This should also include species not bred for commercial export, but kept privately.',
+      text: 'formFour.helpText.formFour',
       isBold: false,
     },
   ],
   formFourOne: [
     {
-      text:
-        'Unusual activity observed during an inspection should be noted. This may include, but not be limited to: denial of inspection officer to an area of the facility (that may be used for housing specimens not permitted in trade, or the facility may not have a license to keep); evidence of shipments of specimens not permitted in trade; and the manager or owner of the facility not being able to provide information about specimens kept at the facility, or know where to locate this information.',
+      text: 'formFour.helpText.formFourOne',
       isBold: false,
     },
   ],
@@ -170,64 +182,55 @@ export default {
   ],
   formFourNew: [
     {
-      text:
-        'Suitable enclosures or containment facilities should provide adequate equipment, structure and space for breeding animals to live. There should be adequate enclosures or containment facilities to produce the number of offspring and/or specimens claimed to be produced by the facility.',
+      text: 'formFour.helpText.formFourNew',
       isBold: false,
     },
   ],
   formFourThree: [
     {
-      text:
-        'Unusual activity observed during an inspection should be noted. This may include, but not be limited to: denial of inspection officer to an area of the facility (that may be used for housing specimens not permitted in trade, or the facility may not have a license to keep); evidence of shipments of specimens not permitted in trade; and the manager or owner of the facility not being able to provide information about specimens kept at the facility, or know where to locate this information.',
+      text: 'formFour.helpText.formFourThree',
       isBold: false,
     },
   ],
   formFourFour: [
     {
-      text:
-        'Maintenance of records is an integral part of managing captive-breeding and/or ranching operations. Genuine ranching and captive breeding facilities usually keep records of all stock, general husbandry practices and treatment of any diseases or injured specimens. Other records may be kept on: Mortality and disposal of dead specimens; Annual production of stock (neonates, eggs, hatching percentages, etc.) Number and source of parental stock; and Age and identification (e.g., band or tag numbers, transponders, distinguishing marks) of specimen. Records may be as simple as hand-written notes in diaries to elaborate online studbooks.',
+      text: 'formFour.helpText.formFourFour',
       isBold: false,
     },
   ],
   formFourFive: [
     {
-      text:
-        'Facilities should provide adequate species-specific food for all life stages of specimens kept. For example, juvenile pythons may require small mice while adults may require large rats or chickens.',
+      text: 'formFour.helpText.formFourFive',
       isBold: false,
     },
   ],
   formFourSix: [
     {
-      text:
-        'Specimens declared as captive-bred from facilities that appear to exceed their breeding capacity is indicative of possible suspicious activity. Inspectors should use the Production Capacity Calculator provided to determine probable output. This question does not relate to ranching facilities, but must be addressed where a facilities claims to undertake any form breeding in a controlled environment.',
+      text: 'formFour.helpText.formFourSix',
       isBold: false,
     },
   ],
   formFourSeven: [
     {
-      text:
-        'Specimens taken from the wild often exhibit symptoms not found in captive specimens, which may alert inspectors to potentially false claims of captive breeding. Such indicators may include: High levels of stress (mammals pacing within cages, raw noses of snakes, high levels of aggression or passivity and torpor); Physical damage (scars on skin of all species, broken turtle carapaces, wounds from snares); and High parasite loads (ticks on snakes, lizards and turtles, lice on mammals and birds) or skin problems (bald/bare areas, scale abnormalities, shell rot, etc.).',
+      text: 'formFour.helpText.formFourSeven',
       isBold: false,
     },
   ],
   formFourEight: [
     {
-      text:
-        'Facilities claiming to produce large numbers of specimens of species known to be difficult to breed and/or maintain in captivity may warrant greater scrutiny than facilities raising easy to keep species.',
+      text: 'formFour.helpText.formFourEight',
       isBold: false,
     },
   ],
   formFourNine: [
     {
-      text:
-        'High volumes of trade in specimens from recently established facilities may indicate suspicious activity.\nThe facility should be established long enough to produce offspring (if claiming the specimens are captive-bred). Examples when this may be the case include species that grow slowly or take many years to reach maturity, and species producing few offspring per litter of clutch.',
+      text: 'formFour.helpText.formFourNine',
       isBold: false,
     },
   ],
   formFourTen: [
     {
-      text:
-        'CITES Appendix I specimens are required to be marked with a permanent identification (see Resolution Conf. 12.10 (Rev. CoP15)). Non-compliance with this requirement may signal other ways in which the facility is not complying.',
+      text: 'fourFour.helpText.formFourTen',
       isBold: false,
     },
   ],
